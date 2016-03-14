da7311d898a10ceaddd5597c12ee11bac9a87ce6
Rishi Nag <rishi@ebi.ac.uk>
2016-03-14 11:11:49 +0000

    Faidx perldoc updates

--- lib/Bio/DB/HTS/Faidx.pm
+++ lib/Bio/DB/HTS/Faidx.pm
@@ -1,3 +1,59 @@
+=head1 LICENSE
+
+Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 NAME
+
+Faidx -- Perl extension for accessing bgzip compressed and indexed FASTA using htslib
+
+=head1 SYNOPSIS
+
+ #include the module
+ use Bio::DB::HTS::Faidx;
+
+ #create the index object
+ my $fasta = "$Bin/data/Saccharomyces_cerevisiae.R64-1-1.dna.chromosome.I.fa.gz" ;
+ my $location = "I:1-100" ;
+ my $seq_id = "I" ;
+ my $index = Bio::DB::HTS::Faidx->new($fasta);
+
+ #get sequence using the location
+ my $seq = "" ;
+ my $length = 0 ;
+ ($seq, $length) = $index->get_sequence($location);
+
+ $seq = $index->get_sequence_no_length($location);
+ $length = $index->length($seq_id);
+
+ #get sequence using the separate parameters.
+ #Note here that the sequence start and end points are zero indexed, so code accordingly.
+ ($seq, $length) = $index->get_sequence2("I",1,99);
+ $seq = $index->get_sequence2_no_length("I",1,99);
+
+ my @seq_ids = $index->get_all_sequence_ids();
+
+ #returns 1 if sequence ID is present, 0 if not
+ my $has_seq = $index->has_sequence('I');
+
+=head1 AUTHOR
+
+Rishi Nag
+
+=cut
+
+
 package Bio::DB::HTS::Faidx;
 
 use 5.014002;
@@ -33,61 +89,3 @@ XSLoader::load('Bio::DB::HTS::Faidx', $VERSION);
 # Preloaded methods go here.
 1;
 __END__
-
-
-=head1 NAME
-
-Faidx - Perl extension for accessing bgzip compressed and indexed FASTA using htslib
-
-=head1 SYNOPSIS
-
-#include the module
-use Bio::DB::HTS::Faidx;
-
-#create the index object
-my $fasta = "$Bin/data/Saccharomyces_cerevisiae.R64-1-1.dna.chromosome.I.fa.gz" ;
-my $location = "I:1-100" ;
-my $seq_id = "I" ;
-my $index = Bio::DB::HTS::Faidx->new($fasta);
-
-#get sequence using the location
-my $seq = "" ;
-my $length = 0 ;
-($seq, $length) = $index->get_sequence($location);
-
-$seq = $index->get_sequence_no_length($location);
-$length = $index->length($seq_id);
-
-#get sequence using the separate parameters.
-#Note here that the sequence start and end points are zero indexed, so code accordingly.
-($seq, $length) = $index->get_sequence2("I",1,99);
-$seq = $index->get_sequence2_no_length("I",1,99);
-
-my @seq_ids = $index->get_all_sequence_ids();
-
-#returns 1 if sequence ID is present, 0 if not
-my $has_seq = $index->has_sequence('I');
-
-
-=head1 AUTHOR
-
-Rishi Nag
-
-=head1 COPYRIGHT AND LICENSE
-
-# Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-=cut
