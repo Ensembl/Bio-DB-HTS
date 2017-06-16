3878f39817ca99a0467660291b68ed6b9dff7f7a
Dan Bolser <dbolser@ebi.ac.uk>
2017-06-16 11:03:27 +0100

    One minor change and one minor bugfix
    The latter bugfix was discovered debugging an issue with wget. For some reason GNU Wget 1.12 built on linux-gnu drops the .zip extension from both downloads, breaking this install script. I don't have a good patch for that, so I'll not submit the hack I have.

--- INSTALL.pl
+++ INSTALL.pl
@@ -93,7 +93,7 @@ info("Performing build in $install_dir");
 info("Checking out HTSlib v".$htslib_version);
 chdir $install_dir;
 my $htslib_archive = $htslib_version.".zip" ;
-my $htslib_archive_url = "https://github.com/samtools/htslib/archive/".$htslib_version.".zip" ;
+my $htslib_archive_url = "https://github.com/samtools/htslib/archive/".$htslib_archive ;
 system "wget ".$htslib_archive_url  ;
 if ( $? == -1 )
 {
@@ -115,7 +115,7 @@ if ( $? == -1 )
 {
   die "Bio-DB-HTS fetch command failed: $!\n";
 }
--f './'.$htslib_archive or die "Could not fetch Bio::DB::HTS archive ".$biodbhts_archive_url ;
+-f './'.$biodbhts_archive or die "Could not fetch Bio::DB::HTS archive ".$biodbhts_archive_url ;
 system "unzip ".$biodbhts_archive ;
 system "mv Bio-DB-HTS-master Bio-DB-HTS" ;
 -d './Bio-DB-HTS' or die "Unzip seems to have failed. Could not find $install_dir/Bio-DB-HTS directory";
