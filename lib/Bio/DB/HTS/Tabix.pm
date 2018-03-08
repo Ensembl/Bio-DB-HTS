b1a97bbf104fe052e095f1701e0e98d85ca010d6
Alessandro Vullo <avullo@ebi.ac.uk>
2018-03-08 17:35:17 +0000

    [ENSWEB-3657, ENSCORESW-2606]. - Null header expected as a string from ensembl-io parser - Emit warning message in case seq region is not found

--- lib/Bio/DB/HTS/Tabix.pm
+++ lib/Bio/DB/HTS/Tabix.pm
@@ -49,19 +49,22 @@ sub new {
 
   my $htsfile = Bio::DB::HTSfile->open($filename);
   my $tabix_index = tbx_open($filename);
+  # use Data::Dumper; print $tabix_index; die;
   die "Couldn't find index for file " . $filename unless $tabix_index;
   my $header = tbx_header($htsfile, $tabix_index);
-  if( $header )
-  {
+  if( $header ) {
     $header = join "\n", @{ $header } ;
+  } else {
+    $header = '';
   }
+  
   chdir($pdir);
 
   my $self = bless {
                     _htsfile => $htsfile,
                     _filename => $filename,
                     _tabix_index => $tabix_index,
-                    _header=> $header,
+                    _header => $header,
                    }, ref $class || $class;
 
   return $self;
@@ -75,16 +78,17 @@ sub query_full {
     $region .= $start;
     $region .= '-'.$end if(defined $end);
   }
+  
   my $iter = tbx_query( $self->{_tabix_index}, $region );
   unless ( $iter ) {
-    #this likely means the chromosome wasn't found in the tabix index, or it couldn't parse the provided region.
+    # this likely means the chromosome wasn't found in the tabix index, or it couldn't parse the provided region.
     my $seqnames_hash = { map { $_ => 1 } @{ $self->seqnames } };
+    
     if ( not exists $seqnames_hash->{ $chr } ) {
-      #$self->log->warn("Specified chromosome '$chr' does not exist in file " . $self->_filename);
-    }
-    else {
+      warn("Specified chromosome '$chr' does not exist in file " . $self->{_filename});
+    } else {
       die "Unable to get iterator for region '$region' in file ". $self->{_filename} . " -- htslib couldn't parse your region string";
-      }
+    }
   }
 
   return Bio::DB::HTS::Tabix::Iterator->new( _tabix_iter => $iter, _htsfile => $self->{_htsfile}, _tabix_index => $self->{_tabix_index} );
@@ -98,7 +102,7 @@ sub query {
     unless ( defined $chr ) {
         die "You must specify a region in the format chr, chr:start or chr:start-end";
     }
-
+    
     if ( defined $end ) {
         die "End in $region is less than the start" if $end < $start;
     }
